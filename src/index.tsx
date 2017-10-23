16ff4943d57f4e3f55102da16c10d2eb84b6d886
Daniel J Harvey <dan.harvey@futurice.com>
2017-10-23 16:17:31 +0100

    Compile index page from Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import {Provider} from 'react-redux';
-import store from './src/redux/store';
-import AppViewContainer from './src/modules/AppViewContainer';
+import store from './redux/store';
+import AppViewContainer from './modules/AppViewContainer';
 
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 
-class PepperoniAppTemplate extends Component {
+export interface Props { }
+export interface State { }
+
+class PepperoniAppTemplate extends Component<Props, State> {
   render() {
     return (
       <Provider store={store}>
