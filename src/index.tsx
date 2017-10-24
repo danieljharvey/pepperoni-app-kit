1182815e5582d7e5c65736a25b06b021f0a45014
Daniel J Harvey <dan.harvey@futurice.com>
2017-10-24 12:23:56 +0100

    Tests fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import {Provider} from 'react-redux';
-import store from './redux/store';
-import AppViewContainer from './modules/AppViewContainer';
+import {Provider} from "react-redux"
+import store from "./redux/store"
+import AppViewContainer from "./modules/AppViewContainer"
 
-import React, {Component} from 'react';
-import {AppRegistry} from 'react-native';
+import React, {Component} from "react"
+import {AppRegistry} from "react-native"
 
 export interface Props { }
 export interface State { }
@@ -14,8 +14,8 @@ class PepperoniAppTemplate extends Component<Props, State> {
       <Provider store={store}>
         <AppViewContainer />
       </Provider>
-    );
+    )
   }
 }
 
-AppRegistry.registerComponent('PepperoniAppTemplate', () => PepperoniAppTemplate);
+AppRegistry.registerComponent("PepperoniAppTemplate", () => PepperoniAppTemplate)
